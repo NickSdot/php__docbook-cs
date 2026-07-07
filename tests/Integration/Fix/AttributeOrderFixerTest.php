0c158169af32220845bc5453e610317407ddbadd
NickSdot <catch.github.com.nicksdot@nicksdot.dev>
2026-07-08 00:29:49 +0700

    refactor: moved attribute order fixer test to dedicated class

---
+++ tests/Integration/Fix/AttributeOrderFixerTest.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DocbookCS\Tests\Integration\Fix;
+
+use DocbookCS\Fix\Fix;
+use DocbookCS\Fix\FixApplier;
+use DocbookCS\Fix\FixResult;
+use DocbookCS\Fix\Fixer\AttributeOrderFixer;
+use DocbookCS\Report\Violation;
+use DocbookCS\Runner\RunMode;
+use DocbookCS\Sniff\AttributeOrderSniff;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+
+#[CoversClass(AttributeOrderFixer::class)]
+#[CoversClass(AttributeOrderSniff::class)]
+#[CoversClass(Fix::class)]
+#[CoversClass(FixApplier::class)]
+#[CoversClass(FixResult::class)]
+#[CoversClass(RunMode::class)]
+#[CoversClass(Violation::class)]
+final class AttributeOrderFixerTest extends TestCase
+{
+    #[Test]
+    public function itMovesXmlIdBeforeXmlns(): void
+    {
+        $content = '<root xmlns="urn:test" xml:id="root"/>';
+        $document = $this->createDocument($content);
+
+        $violations = new AttributeOrderSniff(RunMode::Fix)->process(
+            $document,
+            $content,
+            'file.xml',
+        );
+
+        self::assertCount(1, $violations);
+        self::assertSame('<root xmlns="urn:test" xml:id="root"/>', $violations[0]->content);
+
+        $fix = new AttributeOrderFixer()->process($violations[0]);
+
+        self::assertInstanceOf(Fix::class, $fix);
+
+        $result = new FixApplier()->apply($content, [$fix]);
+
+        self::assertSame('<root xml:id="root" xmlns="urn:test"/>', $result->content);
+        self::assertSame(1, $result->applied);
+    }
+
+    private function createDocument(string $xml): \DOMDocument
+    {
+        $document = new \DOMDocument();
+        $document->loadXML($xml);
+
+        return $document;
+    }
+}
--- tests/Integration/Fix/FixerTest.php
+++ tests/Integration/Fix/FixerTest.php
@@ -4,23 +4,11 @@ declare(strict_types=1);
 
 namespace DocbookCS\Tests\Integration\Fix;
 
-use DocbookCS\Fix\Fix;
-use DocbookCS\Fix\FixApplier;
-use DocbookCS\Fix\FixResult;
-use DocbookCS\Fix\Fixer\AttributeOrderFixer;
-use DocbookCS\Report\Violation;
-use DocbookCS\Runner\RunMode;
-use DocbookCS\Sniff\AttributeOrderSniff;
-use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\CoversNothing;
 use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\TestCase;
 
-#[CoversClass(AttributeOrderFixer::class)]
-#[CoversClass(AttributeOrderSniff::class)]
-#[CoversClass(Fix::class)]
-#[CoversClass(FixApplier::class)]
-#[CoversClass(FixResult::class)]
-#[CoversClass(Violation::class)]
+#[CoversNothing]
 final class FixerTest extends TestCase
 {
     #[Test]
@@ -36,47 +24,6 @@ final class FixerTest extends TestCase
 
         self::assertSame("<root>\n  <tag/>\n</root>", $result->content);
         self::assertSame(2, $result->applied);
-    }
-
-    #[Test]
-    public function attributeOrderFixerMovesXmlIdBeforeXmlns(): void
-    {
-        $content = '<root xmlns="urn:test" xml:id="root"/>';
-        $document = $this->createDocument($content);
-
-        $violations = new AttributeOrderSniff(RunMode::Fix)->process(
-            $document,
-            $content,
-            'file.xml',
-        );
-
-        // fixer isn't yet applied
-        self::assertCount(1, $violations);
-        self::assertSame('<root xmlns="urn:test" xml:id="root"/>', $violations[0]->content);
-
-        $fix = new AttributeOrderFixer()->process($violations[0]);
-
-        self::assertInstanceOf(Fix::class, $fix);
-
-        $result = new FixApplier()->apply($content, [$fix]);
-
-        self::assertSame('<root xml:id="root" xmlns="urn:test"/>', $result->content);
-        self::assertSame(1, $result->applied);
-    }
-
-    #[Test]
-    public function exceptionNameFixerReplacesSimpleClassnameTags(): void
-    {
-        $content = '<root><classname>RuntimeException</classname></root>';
-        $document = $this->createDocument($content);
-
-        $this->markTestIncomplete('ExceptionName fixer is not wired in this pass.');
-
-        // todo
-        $result = (object) ['content' => 'foo', 'applied' => 1];
-
-        self::assertSame('<root><exceptionname>RuntimeException</exceptionname></root>', $result->content);
-        self::assertSame(1, $result->applied);
     }
 
     #[Test]
